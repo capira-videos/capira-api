23383e3ee1b706302c67c59605da933516242c25
Paul Szymanski <mail@pszy.de>
2015-07-29 23:33:32 +0200

    Change how tests get URL

--- test/user_spec.js
+++ test/user_spec.js
@@ -1,21 +1,5 @@
 var frisby = require('frisby');
-
-var URL = 'http://192.168.178.83:8888/api'
-
-/*
-frisby.create('GET channel 1')
-	.get(URL + '/channel/1')
-	.expectStatus(200)
-	.expectHeaderContains('content-type', 'text/html;charset=UTF-8')
-	.expectJSON({
-		id: 1,
-		title: 'Mathe-Crashkurs'
-	}) 
-	.afterJSON( function(channel) {
-		expect(channel.id).toEqual(1);
-	})
-.toss();
-*/
+var URL = process.env.URL
 
 frisby.create('POST login')
 	.post(URL + '/user/login', {
