35008d7197bd5faf0e4113bc30a47ecea957ab3a
Robin Linus <robin_woll@capira.de>
2015-07-30 08:35:26 +0200

    Add Templates and custom 404

--- api/index.php
+++ api/index.php
@@ -1,9 +1,20 @@
 <?php
-require 'vendor/autoload.php';
+/*
+ * If you prefer to use a PHP session,
+ * you must configure and start a native PHP session with session_start()
+ * before you instantiate the Slim application.
+ * ( http://docs.slimframework.com/sessions/native/ )
+ */
+include_once 'common.php';
 
+require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
-include_once 'common.php';
+$view = $app->view();
+$view->setTemplatesDirectory('./templates/');
+$app->notFound(function () use ($app) {
+	$app->render('404.html');
+});
 
 include_once 'routes/user-api.php';
 include_once 'routes/channel-api.php';
