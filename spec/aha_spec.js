4a6cf078d8f201473a0a562f6ed21dbbb5ae3cbd
Paul Szymanski <mail@pszy.de>
2015-07-31 03:18:38 +0200

    Add simple tests using jasmine

--- spec/aha_spec.js
+++ spec/aha_spec.js
@@ -1,16 +1,42 @@
+'use strict';
 var request = require('request');
+var apiUrl = process.env.URL;
+
+
+describe('A User', function() {
+var time = Date.now();
+var user = {
+        name: 'test_'+time,
+        password: '4378dsfa',
+        email:    'test_'+time+'@capira.de'};
 
 
 
-request.get('http://localhost:8888/api/channel/1', function (error, response, body) {
-	console.log(error, response, body);
-});
-
-
-
-
-describe("Hello world", function() {
-  it("says hello", function() {
-    expect(true).toEqual(true);
+  it('can be created', function(done) {
+    request({
+      method: 'POST',
+      uri:    apiUrl + '/signup',
+      json:   true,
+      body:   user
+    }, function (error, response, body) {
+    	expect(response.statusCode).toBe(201);
+    	done();
+	});
   });
+
+  it('cant use existing mail for registration', function(done) {
+    request({
+      method: 'POST',
+      uri:    apiUrl + '/signup',
+      json:   true,
+      body:   user
+    }, function (error, response, body) {
+    	expect(response.statusCode).toBe(406);
+    	done();
+	});
+  });
+
+  
+
+
 });
--- spec/user_specDISABLED.js
+++ spec/user_specDISABLED.js
@@ -1,5 +1,5 @@
 'use strict';
-var frisby = require('frisby');
+var frisby = require('frisby')
 var apiUrl = process.env.URL;
 
 
