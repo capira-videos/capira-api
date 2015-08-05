1248766f272c31bc9099299aae96e4ae5d44dc5f
Robin Linus <robin_woll@capira.de>
2015-08-05 16:35:38 +0200

    Add gulp-todo
    This gulp-tasks parses TODO comments from our sources

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp');
 var apidoc = require('gulp-api-doc');
 var clean = require('gulp-clean');
-var swaggerGenerator = require('gulp-apidoc-swagger');
+var todo = require('gulp-todo');
+
 
 gulp.task('doc', function() {
     return gulp.src('api/routes/')
@@ -31,9 +32,30 @@ gulp.task('copy', ['clean'], function() {
 
 
 
+
+// generate a todo.md from your javascript files 
+gulp.task('todo', function() {
+    gulp.src(['spec/**/*.js', 'api/**/*.php'])
+        .pipe(todo())
+        .pipe(gulp.dest('./')) //output todo.md as markdown 
+        .pipe(todo.reporter('json', {
+            fileName: 'todo.json'
+        }))
+        .pipe(gulp.dest('./')); //output todo.json as json         // -> Will output a TODO.md with your todos 
+});
+
+
+
+
+
+
+/*
+var swaggerGenerator = require('gulp-apidoc-swagger');
+
 gulp.task('swag', function() {
     swaggerGenerator.exec({
         src: "api/routes/",
         dest: "spec/"
     });
 });
+*/
