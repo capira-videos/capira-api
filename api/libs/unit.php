799a950762ee20f1899d9970ed662e7fd42a64af
Pascal <berrang@cs.uni-saarland.de>
2015-09-17 22:36:29 +0200

    change unit apis

--- api/libs/unit.php
+++ api/libs/unit.php
@@ -20,11 +20,12 @@ function getUnit($id, $channel = false) {
 
 	$stmt->execute();
 
-	$unit = get_result($stmt);
+	$data = get_result($stmt);
 	$stmt->fetch();
 
+	$unit = unserialize($data['unitblob']);
+
 	$stmt->close();
-	$unit['layers'] = getLayers($id);
 
 	if ($channel !== false) {
 		// fetch next unit
@@ -57,66 +58,6 @@ function getUnit($id, $channel = false) {
 
 }
 
-function getLayers($id) {
-	global $mysqli, $user;
-	$query = "SELECT l.*, p.successes, p.failures, p.averageTime, p.bestTime, (SELECT AVG(p1.bestScore) FROM Progress p1 WHERE p1.layerId = l.id) AS averageScore
-			  FROM Layers l
-			  LEFT JOIN Progress p ON l.id = p.layerId AND p.userId=?
-			  WHERE l.parent=?
-			  ORDER BY l.start";
-	$stmt = $mysqli->prepare($query);
-	$userid = $user->userid();
-	$stmt->bind_param("ii", $userid, $id);
-	$stmt->execute();
-
-	$layer = get_result($stmt);
-	$layersRaw = array();
-	while ($stmt->fetch()) {
-		$layersRaw[] = $layer;
-		$layer = get_result($stmt);
-	}
-	$stmt->close();
-
-	$layers = array();
-	foreach ($layersRaw as $layer) {
-		$layers[] = getQuiz($layer);
-	}
-
-	return $layers;
-}
-
-function getQuiz($layer) {
-	$layer['layerCSS'] = trim($layer['layerCSS']);
-	$layer['layerJS'] = 'function test(){var x=2}';
-	$layer['items'] = getItems($layer['id']);
-
-	//$layer['progress']=0;//random_0_1();
-	//$layer['tries']=0;
-	//$layer['average']=random_0_1();
-
-	return $layer;
-}
-
-function getItems($id) {
-	global $mysqli;
-	$query = "SELECT * FROM Items WHERE parent=?";
-
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("i", $id);
-
-	$stmt->execute();
-
-	$items = array();
-	$item = get_result($stmt);
-	while ($stmt->fetch()) {
-		$item['caption'] = trim($item['caption']);
-		$items[] = $item;
-		$item = get_result($stmt);
-	}
-
-	return $items;
-}
-
 function addUnitToChannel($unitId, $channelId) {
 	global $mysqli;
 
@@ -129,96 +70,58 @@ function addUnitToChannel($unitId, $channelId) {
 	$stmt->close();
 }
 
-function createLayer($layer, $parent) {
-	global $mysqli;
-	$query = "INSERT INTO Layers(parent,duration,start,type,interaction,classes,layerCSS) VALUES(?,?,?,?,?,?,?) ";
-
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("iddssss", $parent, $layer['duration'], $layer['start'], $layer['type'], $layer['interaction'], $layer['classes'], $layer['layerCSS']);
-
-	$stmt->execute();
-	$layer['id'] = $stmt->insert_id;
-	$stmt->close();
-	foreach ($layer['items'] as $item) {
-		if (!isset($item['deleted']) || !($item['deleted'])) {
-			createItem($item, $layer['id']);
-		}
-	}
-	return $layer;
-}
-
-function createItem($item, $parent) {
-	global $mysqli;
-	$query = " INSERT INTO Items(parent,x,y,height,width,caption,expectedValue,feedback,type,classes) VALUES(?,?,?,?,?,?,?,?,?,?) ";
-
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("iddddsssss", $parent, $item['x'], $item['y'], $item['height'], $item['width'], $item['caption'], $item['expectedValue'], $item['feedback'], $item['type'], $item['classes']);
-
-	$stmt->execute();
-	$stmt->close();
-}
-
 function updateUnit($unit) {
 	global $mysqli;
 
 	check_unit_privileges($unit['id'], AUTHOR);
 
-	$query = "UPDATE Units SET title=?, videoId=?, published=? WHERE id=?";
+	$query = "UPDATE Units SET title=?, videoId=?, published=?, unitblob=? WHERE id=?";
 
 	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("ssii", $unit['title'], $unit['videoId'], $unit['published'], $unit['id']);
 
-	$stmt->execute();
-	$stmt->close();
+	// delete and create
 	if (isset($unit['layers'])) {
+		$layers = array();
+		$maxLayerId = 0;
+		$maxItemId = 0;
+		// determine max id's
+		foreach ($unit['layers'] as $layer) {
+			if(isset($layer['id'])) {
+				$maxLayerId = max($maxLayerId, $layer['id']);
+				foreach ($layer['items'] as $item) {
+					if(isset($item['id'])) {
+						$maxItemId = max($maxItemId, $item['id']);
+					}
+				}
+			}
+		}
+
+		// then delete and set ids
 		foreach ($unit['layers'] as $layer) {
 			if (isset($layer['deleted']) && ($layer['deleted'])) {
-				if (isset($layer['id'])) {
-					deleteLayer($layer['id']);
-				}
 				continue;
 			}
-			if (isset($layer['id'])) {
-				updateLayer($layer);
-			} else {
-				createLayer($layer, $unit['id']);
+			if (!isset($layer['id'])) {
+				$layer['id'] = ++$maxLayerId;
+			}
+			foreach ($layer['items'] as $item) {
+				if (isset($item['deleted']) && ($item['deleted'])) {
+					continue;
+				}
+				if (!isset($item['id'])) {
+					$item['id'] = ++$maxItemId;
+				}
 			}
 		}
 	}
+
+	$blob = serialize($unit);
+	$stmt->bind_param("ssisi", $unit['title'], $unit['videoId'], $unit['published'], $blob, $unit['id']);
+
+	$stmt->execute();
+	$stmt->close();
 	return $unit;
 
-}
-
-function updateLayer($layer) {
-	global $mysqli;
-	$query = "UPDATE Layers SET duration=?, start=?, type=?, interaction=?, classes=?, layerCSS=? WHERE id=?";
-
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("ddssssi", $layer['duration'], $layer['start'], $layer['type'], $layer['interaction'], $layer['classes'], $layer['layerCSS'], $layer['id']);
-
-	$stmt->execute();
-	$stmt->close();
-
-	foreach ($layer['items'] as $item) {
-		if (isset($item['deleted']) && ($item['deleted'])) {
-			deleteItem($item);
-			continue;
-		}
-		if (isset($item['id'])) {
-			updateItem($item);
-		} else {
-			createItem($item, $layer['id']);
-		}
-	}
-}
-
-function updateItem($item) {
-	global $mysqli;
-	$query = "UPDATE Items SET x=?, y=?, height=?, width=?, caption=?, expectedValue=?, feedback=?, type=?, classes=? WHERE id=?";
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("ddddsssssi", $item['x'], $item['y'], $item['height'], $item['width'], $item['caption'], $item['expectedValue'], $item['feedback'], $item['type'], $item['classes'], $item['id']);
-	$stmt->execute();
-	$stmt->close();
 }
 
 function deleteUnit($unitId) {
@@ -244,40 +147,15 @@ function deleteUnitFromChannel($unitId, $channelId) {
 
 }
 
-function deleteLayer($layerId) {
-	global $mysqli;
-	$query = "DELETE FROM Layers WHERE id=?";
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("i", $layerId);
-	$stmt->execute();
-	$stmt->close();
-
-	$query = "DELETE FROM Items WHERE parent=?";
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("i", $layerId);
-	$stmt->execute();
-	$stmt->close();
-}
-
-function deleteItem($item) {
-	global $mysqli;
-	$query = "DELETE FROM Items WHERE id=?";
-
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("i", $item['id']);
-	$stmt->execute();
-	$stmt->close();
-}
-
 function createUnit($unit) {
 	global $mysqli, $user;
 
 	if (isset($unit['parent'])) {
 		$parent = $unit['parent'];
 		check_channel_privileges($parent, AUTHOR);
-		$query = "INSERT INTO Units(title,videoId,authorId,homechannel) VALUES(?,?,?,?) ";
+		$query = "INSERT INTO Units(title,videoId,authorId,homechannel,unitblob) VALUES(?,?,?,?,?) ";
 	} else {
-		$query = "INSERT INTO Units(title,videoId,authorId) VALUES(?,?,?) ";
+		$query = "INSERT INTO Units(title,videoId,authorId,unitblob) VALUES(?,?,?,?) ";
 	}
 
 	$stmt = $mysqli->prepare($query);
@@ -285,10 +163,11 @@ function createUnit($unit) {
 	if (!isset($unit['authorId'])) {
 		$unit['authorId'] = 0;
 	}
+	$blob = serialize($unit);
 	if (isset($unit['parent'])) {
-		$stmt->bind_param("sssi", $unit['title'], $unit['videoId'], $unit['authorId'], $parent);
+		$stmt->bind_param("sssis", $unit['title'], $unit['videoId'], $unit['authorId'], $parent, $blob);
 	} else {
-		$stmt->bind_param("sss", $unit['title'], $unit['videoId'], $unit['authorId']);
+		$stmt->bind_param("ssss", $unit['title'], $unit['videoId'], $unit['authorId'], $blob);
 	}
 
 	$stmt->execute();
@@ -303,10 +182,6 @@ function createUnit($unit) {
 			addUnitToChannel($unit['id'], $unit['parent']);
 		}
 
-	}
-
-	if (isset($unit['layers']) && count($unit['layers']) > 0) {
-		return updateUnit($unit);
 	}
 	return $unit;
 }
